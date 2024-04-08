ebfd30339f667ebfb620002ef803983ccbcbfd56
fahdd95 <fahdakramdaher@gmail.com>
2024-04-08 20:39:03 +0200

    add get time

--- backend/ping_app/ping.py
+++ backend/ping_app/ping.py
@@ -1,4 +1,5 @@
 from flask_restx import Resource, Namespace
+from datetime import datetime
 
 ping_ns = Namespace("Ping")
 
@@ -7,3 +8,10 @@ ping_ns = Namespace("Ping")
 class Ping(Resource):
     def get(self):
         return {"status": "success", "message": "pong!"}
+
+
+@ping_ns.route("/time")
+class Time(Resource):
+    def get(self):
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        return {"status": "success", "server_time": current_time}
