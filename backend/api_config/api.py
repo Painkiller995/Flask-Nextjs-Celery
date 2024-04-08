dcc8d9fb92a5760fb8213823d2a6c721ffe4160f
fahdd95 <fahdakramdaher@gmail.com>
2024-04-08 20:41:51 +0200

    add try except

--- backend/api_config/api.py
+++ backend/api_config/api.py
@@ -1,7 +1,7 @@
 from flask_restx import Api
 from ..ping_app.ping import ping_ns
 
-api = Api(version="1.0", title="API", doc="/api-docs")
+api = Api(version="1.0", title="API", doc="/docs")
 
 
 def add_namespaces(api: Api):
--- backend/ping_app/ping.py
+++ backend/ping_app/ping.py
@@ -7,11 +7,20 @@ ping_ns = Namespace("Ping")
 @ping_ns.route("/ping")
 class Ping(Resource):
     def get(self):
-        return {"status": "success", "message": "pong!"}
+        try:
+            return {
+                "status": "success",
+                "message": "pong!",
+            }, 200
+        except Exception as e:
+            return {"status": "error", "message": str(e)}, 500
 
 
 @ping_ns.route("/time")
 class Time(Resource):
     def get(self):
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        return {"status": "success", "server_time": current_time}
+        try:
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            return {"status": "success", "server_time": current_time}, 200
+        except Exception as e:
+            return {"status": "error", "message": str(e)}, 500
